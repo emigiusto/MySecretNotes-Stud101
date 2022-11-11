5b399fe9d03963de3717ef3e2f1a9f6146909c4f
emicludo <emg@cludo.com>
2022-11-11 14:56:07 +0100

    fix http create server

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var http = require('http');
-var fs = require('fs');
 
 http.createServer(function (req, res) {
   let newDate = new Date();
-  res.send("Date is: " + newDate.toString());
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end("Date is: " + newDate.toString());
 }).listen(3001);
