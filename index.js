851ca6bf99f6e646540dc321edbfc34e2e876b58
emicludo <emg@cludo.com>
2022-11-14 18:26:49 +0100

    add btc price service

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
 var http = require('http');
+const axios = require('axios');
+const API_Key = 'cf9c2c22-086d-4227-883a-878bfe22aad0';
 
 http.createServer(function (req, res) {
   let newDate = new Date();
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end("Date is: " + newDate.toString());
+  
+  let response = null;
+  new Promise(async (resolve, reject) => {
+    try {
+      response = await axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest', {
+        headers: {
+          'X-CMC_PRO_API_KEY': API_Key,
+        },
+      });
+    } catch(ex) {
+      response = null;
+      // error
+      console.log(ex);
+      reject(ex);
+    }
+    if (response) {
+      // success
+      const json = response.data;
+      res.end("Current BTC/USD price is: " + json.data[0].quote.USD.price);
+    }
+  });
+  
 }).listen(3001);
+
+
+
+
