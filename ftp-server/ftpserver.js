0d3b9aeaec819b8e4acc9977a6d6ee7888c0bba7
emicludo <emg@cludo.com>
2022-11-15 22:48:33 +0100

    add

--- ftp-server/ftpserver.js
+++ ftp-server/ftpserver.js
@@ -4,12 +4,12 @@ const FtpSrv = require('ftp-srv');
 const port = 21;
 const ftpServer = new FtpSrv({
     url: "ftp://0.0.0.0:" + port,
-    anonymous: true
+    anonymous: false
 });
 
 ftpServer.on('login', ({ connection, username, password }, resolve, reject) => { 
-    if(username === 'anonymous' && password === 'anonymous'){
-        return resolve({ root:"/" });    
+    if(username === 'attacker' && password === 'attacker'){
+        return resolve({root: '/'});    
     }
     return reject(new errors.GeneralError('Invalid username or password', 401));
 });
